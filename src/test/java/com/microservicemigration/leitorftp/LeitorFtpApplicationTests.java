6afcd87f4ee81fa1680c0e555e763b83762be039
camilasoares <camilasoares1507@gmail.com>
2021-03-24 13:26:40 -0300

    comentando test

--- src/test/java/com/microservicemigration/leitorftp/LeitorFtpApplicationTests.java
+++ src/test/java/com/microservicemigration/leitorftp/LeitorFtpApplicationTests.java
@@ -1,12 +1,11 @@
 package com.microservicemigration.leitorftp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LeitorFtpApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
